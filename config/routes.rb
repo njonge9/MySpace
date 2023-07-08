19c7aef893a616e0bd710b7f0ca4ecf6ce68e909
njonge9 <frednjonge9@gmail.com>
2023-07-08 14:27:05 +0300

    Fix the scaffold error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :titles
+  resources :posts
   root 'pages#home'
   get '/about', to:'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_08_112058) do
-  create_table "titles", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2023_07_08_112557) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
