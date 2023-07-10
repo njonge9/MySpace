2d6af30917a69f1088e8d44f60ca78bdf1af27b8
njonge9 <frednjonge9@gmail.com>
2023-07-10 22:22:40 +0300

    Modify the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   get '/u/:id', to: 'users#profile', as: 'user'
 
-  resources :posts
+  resources :posts do
+    resources :comment
+  end
 
   root 'pages#home'
   get '/about', to: 'pages#about'
