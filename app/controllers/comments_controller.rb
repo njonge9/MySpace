441c1535c02b93f6f22116a7041b87ef42898522
njonge9 <frednjonge9@gmail.com>
2023-07-11 13:06:51 +0300

    Fix action text in rails error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,34 +1,31 @@
 class CommentsController < ApplicationController
     before_action :authenticate_user!
     before_action :set_post
-
+  
     def create
-        @post = Post.find(params[:post_id])
-        @comment = @post.comments.create(comment_params)
-        @comment.user = current_user
-
-        if @commen.save
-            flash[:notice] = "Comment has been created"
-            redirect_to post_path(@post)
-        else
-            flash[:alert] = "Comment has been created"
-            redirect_to post_path(@post)
-        end
+      @comment = @post.comments.create(comment_params)
+      @comment.user = current_user
+  
+      if @comment.save
+        redirect_to post_path(@post), notice: 'Comment has been created'
+      else
+        redirect_to post_path(@post), alert: 'Comment has not been created'
+      end
     end
-
+  
     def destroy
-        @comment = @post.comments.find(params[:id])
-        @comment.destroy
-        redirect_to post_path(@post)
+      @comment = @post.comments.find(params[:id])
+      @comment.destroy
+      redirect_to post_path(@post)
     end
-
+  
     private
-
+  
     def set_post
-        @post = Post.find(params[:post_id])
+      @post = Post.find(params[:post_id])
     end
-
+  
     def comment_params
-        params.require(:comment).permit(:body)
+      params.require(:comment).permit(:body)
     end
-end
+  end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,5 @@ class Post < ApplicationRecord
     validates :title, uniqueness: true, presence: true, length: { maximum:100, minimum: 5}
     validates :body, presence: true, length: { maximum:200, minimum:10}
     has_many :comments, dependent: :destroy
-    
     belongs_to :user
 end
